292a82909b3d861692bc1ffc22b44e4383808197
Giovannyi Medina <72289144+giovannyi59@users.noreply.github.com>
2022-05-15 13:19:27 -0500

    corregir letra

--- model/token.model.js
+++ model/token.model.js
@@ -4,4 +4,4 @@ const tokenSchema = new mongoose.Schema({
     token: {type:String}
 });
 
-module.exports = mongoose.model('Token', tokenSchema);
+module.exports = Mongoose.model('Token', tokenSchema);
